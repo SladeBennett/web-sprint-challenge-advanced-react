695319819ad8f5cc32c8d1c2938f8f535db64cdc
SladeBennett <slade8colt@gmail.com>
2024-04-28 19:47:42 -0600

    directions semi op, getXY filled

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,27 +1,37 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-// Suggested initial states
- const initialMessage = ''
- const initialEmail = ''
- const initialSteps = 0
- const initialIndex = 4
+import { render } from '@testing-library/react'
+
+
+const initialMessage = ''
+const initialEmail = ''
+const initialSteps = 0
+const initialIndex = 4
 
 export default function AppFunctional(props) {
- const [ message, setMessage ] = useState(initialMessage)
- const [ email, setEmail ] = useState(initialEmail)
- const [ steps, setSteps ] = useState(initialSteps)
- const [ index, setIndex ] = useState(initialIndex)
- 
-  function getXY() {
-    
+  const [message, setMessage] = useState(initialMessage)
+  const [email, setEmail] = useState(initialEmail)
+  const [steps, setSteps] = useState(initialSteps)
+  const [index, setIndex] = useState(initialIndex)
+
+  let gridArr = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+
+  function getXY(idx) {
+    let x = (idx % 3) + 1
+    let y = (Math.floor(idx / 3) + 1)
+    getXYMessage(x, y)
+    console.log(x, y)
     // It's enough to know what index the "B" is at, to be able to calculate the coordinates.
   }
 
-  function getXYMessage() {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
+  function getXYMessage(x, y) {
+    let message = `Coordinates: (${x}, ${y})`
+    return message;
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
   }
+
 
   function reset() {
     setMessage(initialMessage)
@@ -31,12 +41,43 @@ export default function AppFunctional(props) {
   }
 
   function getNextIndex(direction) {
+
+    if (direction === 'left') {
+      if (index - 1 > -1) {
+        setSteps(steps + 1)
+        setIndex(index - 1)
+      }
+      getXY(index - 1)
+    }
+    if (direction === 'right') {
+      if (index + 1 < 9) {
+        setSteps(steps + 1)
+        setIndex(index + 1)
+      }
+      getXY(index + 1)
+    }
+    if (direction === 'up') {
+      if (index - 3 > -1) {
+        setIndex(index - 3)
+        setSteps(steps + 1)
+      }
+      getXY(index - 3)
+    }
+    if (direction === 'down') {
+      if (index + 3 < 9) {
+        setIndex(index + 3)
+        setSteps(steps + 1)
+      }
+      getXY(index + 3)
+    }
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
   }
 
   function move(evt) {
+    let direct = evt.target.id
+    getNextIndex(direct)
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
   }
@@ -48,7 +89,7 @@ export default function AppFunctional(props) {
 
   function onSubmit(evt) {
     evt.preventDefault()
-    
+
     setEmail(initialEmail)
     // Use a POST request to send a payload to the server.
   }
@@ -56,14 +97,14 @@ export default function AppFunctional(props) {
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">{getXYMessage()}</h3>
+        <h3 id="steps">You moved {steps} times</h3>
       </div>
       <div id="grid">
         {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
+          gridArr.map(idx => (
+            <div key={idx} className={`square${idx === index ? ' active' : ''}`}>
+              {idx === index ? 'B' : null}
             </div>
           ))
         }
