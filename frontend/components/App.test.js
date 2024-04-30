af3cd55a7c3b93047e26f6bdccdaaa4e8981f39e
SladeBennett <slade8colt@gmail.com>
2024-04-29 19:42:51 -0600

    complete

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,5 +1,12 @@
 import AppFunctional from "./AppFunctional"
 // Write your tests here
+  let up = document.querySelector('#up')
+  let down = document.querySelector('#down')
+  let left = document.querySelector('#left')
+  let right = document.querySelector('#right')
+  let reset = document.querySelector('#reset')
+  let submit = document.querySelector('#submit')
+  
 test('sanity', () => {
   // expect(true).toBe(false)
 })
